e001b520101d26810f50f2064da55b51218e599e
Paulo Custodio <pauloscustodio@gmail.com>
2016-09-20 00:28:40 +0100

    Correct handling of empty string constructor
    When called with path(""), m_path was defined with size 1, containing one empty string.
    Fix to hold an empty m_path in this case.

--- filesystem/path.h
+++ filesystem/path.h
@@ -328,13 +328,15 @@ protected:
         std::string::size_type lastPos = 0, pos = string.find_first_of(delim, lastPos);
         std::vector<std::string> tokens;
 
-        while (lastPos != std::string::npos) {
-            if (pos != lastPos)
-                tokens.push_back(string.substr(lastPos, pos - lastPos));
-            lastPos = pos;
-            if (lastPos == std::string::npos || lastPos + 1 == string.length())
-                break;
-            pos = string.find_first_of(delim, ++lastPos);
+        if (!string.empty()) {
+            while (lastPos != std::string::npos) {
+                if (pos != lastPos)
+                    tokens.push_back(string.substr(lastPos, pos - lastPos));
+                lastPos = pos;
+                if (lastPos == std::string::npos || lastPos + 1 == string.length())
+                    break;
+                pos = string.find_first_of(delim, ++lastPos);
+            }
         }
 
         return tokens;
